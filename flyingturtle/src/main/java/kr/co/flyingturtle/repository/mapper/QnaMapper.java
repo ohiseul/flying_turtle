7e04bddf65e9e2df35355312e5c7ad80dec96d44
ohiseul <46315956+ohiseul@users.noreply.github.com>
2019-05-07 15:43:17 +0900

    경로정정

--- flyingturtle/src/main/java/kr/co/flyingturtle/repository/mapper/QnaMapper.java
+++ flyingturtle/src/main/java/kr/co/flyingturtle/repository/mapper/QnaMapper.java
@@ -1,12 +1,4 @@
 package kr.co.flyingturtle.repository.mapper;
 
-import java.util.List;
-
-import kr.co.flyingturtle.repository.vo.Qna;
-
 public interface QnaMapper {
-	List<Qna> selectBoard() throws Exception;
-	void insertBoard(Qna board) throws Exception;
-	void updateBoard(Qna board)throws Exception;
-	void deleteBoard(int no)throws Exception;
 }
