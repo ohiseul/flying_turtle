9243f11bb5b945ce33d24b73c7a5236129c10ae0
CHOIJOOHEE <wngml1123@gmail.com>
2019-05-13 09:59:54 +0900

    검색기능시도3

--- flyingturtle/src/main/java/kr/co/flyingturtle/edu/notice/service/NoticeServiceImpl.java
+++ flyingturtle/src/main/java/kr/co/flyingturtle/edu/notice/service/NoticeServiceImpl.java
@@ -19,22 +19,16 @@ public class NoticeServiceImpl implements NoticeService {
 
 	//	전체조회
 	public Map<String, Object> list(Page page) {
-		System.out.println("list.do - ServiceImpl 호출");
-		System.out.println("page VO" + page.getKeyword());
 		Map<String, Object> result = new HashMap<>();
-		System.out.println("Map 생성 후 ---");
-//		System.out.println("page검색어" + page.getKeyword());
+		
+		System.out.println("검색어 : "+page.getKeyword());
 		
 		result.put("keyword",page.getKeyword());
-		System.out.println(mapper.selectListNotice(page).toString());
+		result.put("searchType",page.getSearchType());
 		result.put("lists", mapper.selectListNotice(page));
 		System.out.println("lists 성공");
-//		System.out.println("DB??? "+result.toString());
-//		System.out.println("검색어 : "+page.getKeyword());
-		System.out.println("keyword put 성공");
 		
-		System.out.println("service keyword받기 "+page.getKeyword());
-		result.put("page",new PageResult(page.getPageNo(),mapper.selectNoticeCount()));
+		result.put("page",new PageResult(page.getPageNo(),mapper.selectNoticeCount(page)));
 		System.out.println(result);
 		return result;
 	}
--- flyingturtle/src/main/webapp/resources/js/notice/list.js
+++ flyingturtle/src/main/webapp/resources/js/notice/list.js
@@ -36,7 +36,7 @@ $('.search span').click(function(e) {
 	console.log(url);
 });*/
 
-$("#searchButton").click(function() {
+/*$("#searchButton").click(function() {
 	searchList();
 });
 
@@ -46,5 +46,5 @@ function searchList() {
 	
 	//검색버튼을 클릭할 때마다 1번째 페이지를 보여주기 위해 현재페이지의 값을 1로 고정한다.
 	window.location.href="/notice/list.do?pageNo=1&keyword="+ keyword;
-}
+}*/
 
