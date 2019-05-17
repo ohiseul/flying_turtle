c18e1303165e9ad029a3c1b0f2b0a028ae80652b
bonhyun1106 <qpt3154@gmail.com>
2019-05-17 17:40:38 +0900

    용어사전 - NodeJS로 구현 예정. 프로젝트 폴더 바로 밑에 node_modules깔아서 사용가능. 단! 깃에는 절대 올리지 말것.

---
+++ flyingturtle/src/main/webapp/NodeJS/server.js
@@ -0,0 +1,15 @@
+const http = require("http");
+const port = 10001;
+
+const fs = require("fs");
+
+
+// 서버 생성
+http.createServer(function (request, response) {
+    fs.readFile("dictionary.html", function (err, data) {
+        response.writeHead(200, {"Content-Type":"text/html; charset=utf-8"});
+        response.end(data);
+    });
+}).listen(port, function () {
+    console.log("이게 바로 NodeJS");
+});
