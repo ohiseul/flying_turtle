24243fed528f29f8f6322acd3463b475c32d3f1a
OHISEUL <dltmf3140@gmail.com>
2019-05-18 16:50:03 +0900

    video 등록성공

--- flyingturtle/src/main/java/kr/co/flyingturtle/edu/user/video/VideoController.java
+++ flyingturtle/src/main/java/kr/co/flyingturtle/edu/user/video/VideoController.java
@@ -1,15 +1,56 @@
 package kr.co.flyingturtle.edu.user.video;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+import kr.co.flyingturtle.edu.user.video.service.VideoService;
 import kr.co.flyingturtle.repository.vo.Video;
 
-
+@Controller("kr.co.flyingturtle.edu.user.video.controller")
+@RequestMapping("/user/video")
 public class VideoController {
-
-	@RequestMapping("user/video/videowrite.do")
+	
+	@Autowired	
+	public VideoService service;
+	
+	/*리스트*/
+	@RequestMapping("/videolist.do")
+	@ResponseBody
+	public List<Video> list() throws Exception{
+		   return service.list();
+	}	
+	/*등록*/
+	@RequestMapping("/videowrite.do")
+	@ResponseBody
 	public String write(Video video) throws Exception{
-		System.out.println("왔어");
-		return video.getVideoAddr();
+			service.write(video);
+			System.out.println(video.getContent());
+			System.out.println(video.getTitle());
+			System.out.println(video.getVideoAddr());
+			  return UrlBasedViewResolver.REDIRECT_URL_PREFIX+"video.html";
 	}
+	/*글 하나 가져오기*/
+	@RequestMapping("/videoupdateform.do")
+	@ResponseBody
+	public Video updateform(int videoNo) throws Exception{
+		return service.updateform(videoNo);
+	}
+	/*수정*/
+	@RequestMapping("/videoupdate.do")
+	@ResponseBody
+	public void update(Video video) throws Exception{
+		service.update(video);
+	}
+	/*삭제*/	
+	@RequestMapping("/videodelete.do")
+	@ResponseBody
+	public void delete(int videoNo) throws Exception{
+		service.delete(videoNo);
+	}
+	
 }
--- flyingturtle/src/main/java/kr/co/flyingturtle/edu/user/video/service/VideoService.java
+++ flyingturtle/src/main/java/kr/co/flyingturtle/edu/user/video/service/VideoService.java
@@ -1,5 +1,19 @@
 package kr.co.flyingturtle.edu.user.video.service;
 
-public interface VideoService {
+import java.util.List;
 
+import kr.co.flyingturtle.repository.vo.Video;
+
+
+public interface VideoService {
+	/* 리스트 가져오기*/
+	List<Video> list() throws Exception;
+	/*상세*/
+	Video updateform(int videoNo) throws Exception;
+	/*등록*/
+	void write(Video video) throws Exception;
+	/*수정*/
+	void update(Video video) throws Exception;
+	/*삭제*/
+	public void  delete(int videoNo) throws Exception;
 }
