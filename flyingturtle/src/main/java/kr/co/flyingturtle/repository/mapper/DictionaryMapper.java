8b5971ab2a37c2393e0a71be13fa379f677a4da5
bonhyun1106 <qpt3154@gmail.com>
2019-05-21 20:36:38 +0900

    EDIT DICTIONARY MAPPER COMMENT

--- flyingturtle/src/main/java/kr/co/flyingturtle/repository/mapper/DictionaryMapper.java
+++ flyingturtle/src/main/java/kr/co/flyingturtle/repository/mapper/DictionaryMapper.java
@@ -6,13 +6,13 @@ import kr.co.flyingturtle.repository.vo.Dictionary;
 
 public interface DictionaryMapper {
 	
-	// 조회
+	// 용어 사전 조회
 	Dictionary selectDicWord(int dicNo);
 	
-	// 최초 등록
+	// 소과목 추가시 용어 사전 생성
 	void insertDicWord(Dictionary dic);
 	
-	// 등록
+	// 용어 사전 작성/수정
 	void updateDicWord(Dictionary dic);
 	
 	//과목명 전체 조회
@@ -23,4 +23,5 @@ public interface DictionaryMapper {
 	
 	//소과목명 등록
 	void insertSmallSubject(Dictionary dic);
+	
 }
