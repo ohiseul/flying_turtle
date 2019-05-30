0d15ecfac7e96e0a6a9eaead91358e7a9b6cf028
bonhyun1106 <qpt3154@gmail.com>
2019-05-30 20:32:41 +0900

    파이썬 호출 - 크롤링 자동화

---
+++ flyingturtle/src/main/java/kr/co/flyingturtle/edu/batch/SpringBatch.java
@@ -0,0 +1,29 @@
+package kr.co.flyingturtle.edu.batch;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+@Component
+public class SpringBatch extends HandlerInterceptorAdapter {
+	
+	// 6시마다 크롤링 자동화
+	@Scheduled(cron = "0 00 06 * * *")
+	public void execPythonJobData() {
+
+		String comm = "python C://bit2019//flying_turtle//python//crawling.py";
+		Runtime rt = Runtime.getRuntime();
+		
+		Process p;
+		try {
+			p = rt.exec(comm);
+			p.getErrorStream().close();
+			p.getInputStream().close();
+			p.getOutputStream().close();
+			p.waitFor();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
