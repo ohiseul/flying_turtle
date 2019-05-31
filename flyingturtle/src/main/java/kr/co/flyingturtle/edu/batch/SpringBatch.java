e2a3b5dc2748bdbe8931d6ff113d7b47d8301c65
bonhyun1106 <qpt3154@gmail.com>
2019-05-31 18:09:09 +0900

    스프링 Batch 6시마다 자동 크롤링

--- flyingturtle/src/main/java/kr/co/flyingturtle/edu/batch/SpringBatch.java
+++ flyingturtle/src/main/java/kr/co/flyingturtle/edu/batch/SpringBatch.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 public class SpringBatch extends HandlerInterceptorAdapter {
 	
 	// 6시마다 크롤링 자동화
-	@Scheduled(cron = "0 00 14 * * *")
+	@Scheduled(cron = "0 00 06 * * *")
 	public void execPythonJobData() {
 
 		String comm = "python C://bit2019//flying_turtle//python//crawling.py";
