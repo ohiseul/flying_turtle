d0917e0af6613182adc61c5ca5b63cd29538de49
bonhyun1106 <qpt3154@gmail.com>
2019-06-03 13:04:16 +0900

    채용정보 화면

--- flyingturtle/src/main/java/kr/co/flyingturtle/repository/vo/Employment.java
+++ flyingturtle/src/main/java/kr/co/flyingturtle/repository/vo/Employment.java
@@ -3,25 +3,40 @@ package kr.co.flyingturtle.repository.vo;
 import java.util.Date;
 
 public class Employment {
-//	private int empNo;
-//	private String title;
-//	private String url;
-//	private Date regDate;
-//	private String endDate;
-	
-	private String url;
+	private int empNo;
 	private String title;
-	public String getUrl() {
-		return url;
+	private String url;
+	private Date regDate;
+	private String endDate;
+	public int getEmpNo() {
+		return empNo;
 	}
-	public void setUrl(String url) {
-		this.url = url;
+	public void setEmpNo(int empNo) {
+		this.empNo = empNo;
 	}
 	public String getTitle() {
 		return title;
 	}
 	public void setTitle(String title) {
 		this.title = title;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public Date getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+	public String getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
 	}
 	
 	
