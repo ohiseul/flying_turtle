49d260fa250d085ba6f9af3b45af530ae6b57266
최주희 <wngml4105@naver.com>
2019-06-05 18:41:20 +0900

    ajax 시간만 나오도록

--- flyingturtle/src/main/webapp/resources/admin/js/attend/dayList.js
+++ flyingturtle/src/main/webapp/resources/admin/js/attend/dayList.js
@@ -31,10 +31,10 @@ $('.search span').click(function(e) {
   });
 
   $(".search span").click(function() {
-	if ($("#searchType").show()){
-		$("#searchType").hide();
-	}
-	$("#searchType").show();
+   if ($("#searchType").show()){
+      $("#searchType").hide();
+   }
+   $("#searchType").show();
 });
 /**
 Remove active class on submit
@@ -77,27 +77,27 @@ $("#searchType").show();
 });
 
 $("#searchButton").click(function() {
-	 var searchType=$("#searchType[name=searchType]").val();
-	 var keyword = $("#search").val();
-	 var date = $('#date').val();
-	$.ajax({
-		url:"ajaxDayList.do",
-		data:{searchType:searchType,
-			  keyword:keyword,
-			  attendRegDate:date
-			  },
-		dataType:"json"
-	})
-	.done(function(result){
-		getAttendList(result);
-	});
+    var searchType=$("#searchType[name=searchType]").val();
+    var keyword = $("#search").val();
+    var date = $('#date').val();
+   $.ajax({
+      url:"ajaxDayList.do",
+      data:{searchType:searchType,
+           keyword:keyword,
+           attendRegDate:date
+           },
+      dataType:"json"
+   })
+   .done(function(result){
+      getAttendList(result);
+   });
 });
 
 // chart
 google.charts.load('current', {packages: ['corechart', 'bar']});
 google.charts.setOnLoadCallback(drawBasic);
 
-function drawBasic() {
+function drawBasic(result) {
 
       var data = new google.visualization.DataTable();
       data.addColumn('timeofday', '시간');
@@ -105,10 +105,10 @@ function drawBasic() {
 
       data.addRows([
      
-        [{v: [9, 0, 0], f: '9 am'}, 2],
-        [{v: [10, 0, 0], f:'10 am'}, 3],
-        [{v: [11, 0, 0], f: '11 am'}, 4],
-        [{v: [12, 0, 0], f: '12 pm'}, 5],
+        [{v: [9, 0 ], f: '9 am'}, 2],
+        [{v: [10, 0], f:'10 am'}, 3],
+        [{v: [11, 0], f: '11 am'}, 4],
+        [{v: [12, 0], f: '12 pm'}, 5],
       ]);
 
       var options = {
@@ -119,12 +119,16 @@ function drawBasic() {
           format: 'h:mm a',
           
           viewWindow: {
-            min: [8, 00, 0],
-            max: [12, 30, 0]
+            min: [8, 30, 0],
+            max: [12, 00, 0]
           }
         },
         vAxis: {
-          title: '인원수'
+          title: '인원수',
+          viewWindow: {
+              min: 0,
+              max: 10
+          }
         },
         bar: { groupWidth: '30%' },
       };
@@ -137,31 +141,31 @@ function drawBasic() {
 
 //월, 일 버튼 누르면 ajax로 페이지 이동
 $("#monthBtn").click(function() {
-	location.href="monthList.do"
+   location.href="monthList.do"
 });
 $("#dayBtn").click(function() {
-	location.href="dayList.do"
+   location.href="dayList.do"
 });
 
 
 // 날짜 누르면 해당 날짜에 해당하는 리스트 보여주기
 $("#date").change(function(){
-	var date = $('#date').val();
-	$.ajax({
-		url:"ajaxDayList.do",
-		dataType:"json",
-		data:{attendRegDate:date}
-	})
-	.done(function(result){
-		getAttendList(result);
-		});
+   var date = $('#date').val();
+   $.ajax({
+      url:"ajaxDayList.do",
+      dataType:"json",
+      data:{attendRegDate:date}
+   })
+   .done(function(result){
+      getAttendList(result);
+      });
 });
 
 function getAttendList(result){
-	console.dir(result);
-		html="";
-		html+=
-	`<table class="table">
+   console.dir(result);
+      html="";
+      html+=
+   `<table class="table">
         <tr>
             <th>전체</th>
             <th>출석</th>
@@ -170,15 +174,15 @@ function getAttendList(result){
             <th>결석</th>
         </tr>
         <tr>
-			<td>${result.count}명</td>
-        	
-	        <td>${result.addCount.checkInCount}명</td>
-	    	<td>${result.addCount.lateCount}명</td>
-	    	<td>${result.addCount.earlyCount}명</td>
-	    	<td>${result.addCount.absentCount}명</td>
+         <td>${result.count}명</td>
+           
+           <td>${result.addCount.checkInCount}명</td>
+          <td>${result.addCount.lateCount}명</td>
+          <td>${result.addCount.earlyCount}명</td>
+          <td>${result.addCount.absentCount}명</td>
         </tr>
-        	</table>`;
-	  html+=`<table class="content_table">
+           </table>`;
+     html+=`<table class="content_table">
         <tr>
             <th style="width:50px;"><input type="checkbox"></th>
             <th style="width:60px;">번호</th>
@@ -189,21 +193,20 @@ function getAttendList(result){
             <th>관리</th>
             <th>특이사항</th>
         </tr>`;
-	   for(let i =0; i<result.list.length;i++){
-		  let data = result.list[i];
-		  var checkIn = new Date(data.checkIn).toLocaleString();
-		  var checkOut = new Date(data.checkOut).toLocaleString();
-		  console.log("checkIn",checkIn);
-		  console.log(data);
-		  html +=`<tr>  
+      for(let i =0; i<result.list.length;i++){
+        let data = result.list[i];
+        var checkIn = new Date(data.checkIn);
+        var checkOut = new Date(data.checkOut);
+        console.log(data);
+        html +=`<tr>  
             <td style="width:50px;"><input type="checkbox"></td>
             <td style="width:60px;">${data.attendNo}</td>
             <td>${data.name}</td>
-            <td>${checkIn}</td>
-            <td>${checkOut}</td>
+            <td>${checkIn.getHours()+':'+checkIn.getMinutes()}</td>
+            <td>${checkOut.getHours()+':'+checkOut.getMinutes()}</td>
             <td>${data.codeName}</td>
             <td>
-                <select id="state" name="codeNo">
+                <select class="state" name="codeNo">
                     <option >변경</option>
                     <option value="20">출석</option>
                     <option value="21">지각</option>
@@ -213,10 +216,9 @@ function getAttendList(result){
             </td>`;
            html+= `<td><input id="memo" type="text" placeholder="상태변경 이유를 적어주세요" value="${data.specialNote}"></td>
         </tr>`;
-		 }
-	   $(".tableDiv").html(html);
+       }
+      $(".tableDiv").html(html);
 };
-
 
 //체크박스 모두 체크
 $("#check").click(function(){
@@ -230,47 +232,49 @@ $("#check").click(function(){
     }
 });
 
-
 //저장버튼 누르면 상태, 특이사항 수정
 $("#saveBtn").click(function() {
-	alert("왔음");
-	var checkArr = [];
-	var selectArr =[];
-	var memoArr =[];
-//	체크박스
-//	var memberNo = $("input[name=memberNo]:checked").val();
-	var checkList = $("input[name=memberNo]");
-	var selectList = $("select[name=codeNo]");
-	var memoList = $("input[name=memo]");
-		for(var i = 0; i<checkList.length;i++){
-			if(checkList[i].checked){
-				checkArr.push(checkList[i].value);
-				selectArr.push(selectList[i].value);
-				memoArr.push(memoList[i].value);
-		}
-		
-	}
-		console.log(checkArr);
-		console.log(selectArr);
-		console.log(memoArr);
-	$.ajax({
-		url:"updateState.do",
-		traditional:true,
-		data:{
-			  checkArr:checkArr,
-			  selectArr:selectArr,
-			  memoArr:memoArr}
-	})
-	.done(function(result){
-		console.log(result);
-		console.log(result.addCount.checkInCount);
-		$("#all").text(result.count+"명");
-		$("#checkIn").text(result.addCount.checkInCount+"명");
-		$("#late").text(result.addCount.lateCount+"명");
-		$("#early").text(result.addCount.earlyCount+"명");
-		$("#absent").text(result.addCount.absentCount+"명");
-	});
+   alert("왔음");
+   var checkArr = [];
+   var selectArr =[];
+   var memoArr =[];
+//   체크박스
+//   var memberNo = $("input[name=memberNo]:checked").val();
+   var checkList = $(".content_table input[name=memberNo]");
+   var selectList = $(".content_table select[name=codeNo]");
+   var memoList = $(".content_table input[name=memo]");
+      for(var i = 0; i<checkList.length;i++){
+         if(checkList[i].checked){
+            checkArr.push(checkList[i].value);
+            selectArr.push(selectList[i].value);
+            memoArr.push(memoList[i].value);
+      }
+      
+   }
+      console.log(checkArr);
+      console.log(selectArr);
+      console.log(memoArr);
+   $.ajax({
+      url:"updateState.do",
+      traditional:true,
+      data:{
+           checkArr:checkArr,
+           selectArr:selectArr,
+           memoArr:memoArr}
+   })
+   .done(function(result){
+      console.log(result);
+      $("#all").text(result.count+"명");
+      $("#checkIn").text(result.addCount.checkInCount+"명");
+      $("#late").text(result.addCount.lateCount+"명");
+      $("#early").text(result.addCount.earlyCount+"명");
+      $("#absent").text(result.addCount.absentCount+"명");
+      for(let i = 0; i<checkList.length;i++){
+         if(checkList[i].checked){
+            checkList.prop("checked",false);
+         }
+      }
+   });
 });
-
 
 
