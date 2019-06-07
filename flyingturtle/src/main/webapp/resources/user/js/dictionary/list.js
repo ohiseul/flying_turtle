ff4d48cdc085f6cf5710b3f5a66525954b0772ee
bonhyun1106 <qpt3154@gmail.com>
2019-06-07 11:43:14 +0900

    용어사전 - css 로딩

--- flyingturtle/src/main/webapp/resources/user/js/dictionary/list.js
+++ flyingturtle/src/main/webapp/resources/user/js/dictionary/list.js
@@ -1,10 +1,3 @@
-$('head').append(
-		`<link rel="stylesheet" type="text/css" href="/flyingturtle/resources/user/css/dictionary/list.css">
-		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" 
-		integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" 
-		crossorigin="anonymous">`
-);
-
 $(document).ready( function() {
 	$("main").hide();					// editorJS 숨기기
 	$(".buttonList ul > li").hide();	// 과목 하위메뉴 접은 상태로 로딩
