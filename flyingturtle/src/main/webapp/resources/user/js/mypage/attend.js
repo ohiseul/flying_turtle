1eae0934ff880150c6e93fe6e50df33a98fd265e
최주희 <wngml4105@naver.com>
2019-06-10 16:22:18 +0900

    사용자 ajax 구현 시도 -2

--- flyingturtle/src/main/webapp/resources/user/js/mypage/attend.js
+++ flyingturtle/src/main/webapp/resources/user/js/mypage/attend.js
@@ -1,8 +1,9 @@
 
 
 /*출석현황 보기*/
-$(".attendance").click(function(){  
-	alert("클릭함");
+$(".attendance").click(function(){ 
+	
+	
 	let memberNo = $("input[name='memberNo']").val();
 	console.log(memberNo);
 	$.ajax({
@@ -15,18 +16,23 @@ $(".attendance").click(function(){
 			console.log(result,"result");
 			html="";
 			for(let i = 0; i<result.length;i++){
-				console.log(result[i].codeName);
+				let data = result[i];
+				var checkIn = new Date(data.checkIn);
+				var checkOut = new Date(data.checkOut);
+				var regDate = new Date(data.attendRegDate).toLocaleDateString();
 				html +=`<div class="attend">
-					<span>${result[i].attendRegDate}</span>
-					<div>${result[i].checkIn}</div>
-					<div>${result[i].checkOut}</div>
-					<div>${result[i].codeName}</div>
+					<span id="regdate"><h5>${regDate}</h5></span>
+					<div><h6>입실:${checkIn.getHours()+':'+checkIn.getMinutes()}</h6></div>
+					<div><h6>퇴실:${checkOut.getHours()+':'+checkOut.getMinutes()}</h6></div>
+					<div id="codeName"><h5>${data.codeName}</h5></div>
 					</div>`;
 			}
 			$(".attend-area").html(html);
+			page(3);
 		}
 	});
 });
+
 
 
 
