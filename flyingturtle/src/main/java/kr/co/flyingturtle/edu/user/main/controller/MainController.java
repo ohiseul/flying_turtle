c4e10aaa0f737c7b24340c1087500917b0db8747
최주희 <wngml4105@naver.com>
2019-06-21 14:23:14 +0900

    출석 메인 차트 구현

--- flyingturtle/src/main/java/kr/co/flyingturtle/edu/user/main/controller/MainController.java
+++ flyingturtle/src/main/java/kr/co/flyingturtle/edu/user/main/controller/MainController.java
@@ -1,5 +1,7 @@
 package kr.co.flyingturtle.edu.user.main.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +12,7 @@ import kr.co.flyingturtle.edu.user.attend.service.AttendService;
 import kr.co.flyingturtle.edu.user.employment.service.EmploymentService;
 import kr.co.flyingturtle.edu.user.notice.service.NoticeService;
 import kr.co.flyingturtle.repository.vo.Attend;
+import kr.co.flyingturtle.repository.vo.Member;
 
 @Controller("kr.co.flyingturtle.edu.user.main.controller.MainController")
 @RequestMapping("/user/main")
@@ -21,13 +24,17 @@ public class MainController {
 	private NoticeService notice;
 	@Autowired
 	private AttendService attend;
-	
+	@Autowired
+	private HttpSession session;
+	Member mem;
 	
 	// 로그인 성공시
 	@RequestMapping("/main2.do")
 	public void main2(Model model) {
 		model.addAttribute("noticeList", notice.noticeMain());
 		model.addAttribute("empList", emp.selectEmpList());
+		mem = (Member)session.getAttribute("user");
+		model.addAttribute("pieChart",attend.selectAttendCount(mem.getMemberNo()));
 	}
 	
 	@RequestMapping("/attendCnt.do") 
@@ -36,6 +43,7 @@ public class MainController {
 		System.out.println("attendCnt 컨트롤러 옴"); 
 	    return attend.attendCnt(memberNo);
 	 }
+	
 	
 
 		
